b6146a0496d47417a0937d08b4144a4b517e9937
Junior <martoniojunior@hotmail.com>
2018-06-04 22:53:26 -0300

    Deploy for Heroku #7

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+//Install express server
 const express = require('express');
 const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/index.html'));
+res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
