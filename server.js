34da7c14b9196aad71065984f5dd5a9cae81691a
Junior <martoniojunior@hotmail.com>
2018-06-04 22:09:07 -0300

    Edits for Heroku

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+res.sendFile(path.join(__dirname+'app.component.html'));
 });
 
 app.listen(process.env.PORT || 8080);
