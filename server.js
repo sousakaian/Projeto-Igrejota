dd80e7c0f8040f7dfc66f776e935028905711981
Junior <martoniojunior@hotmail.com>
2018-06-04 22:24:21 -0300

    Deploy for Heroku #2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.static(__dirname));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/app.component.html'));
+res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
